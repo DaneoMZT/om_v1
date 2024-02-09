f7ba9f3f8c5a7873c1e78e3378940499c4f17a27
DaneoMZT <danielruizbeltran@MacBook-Air-de-Daniel.local>
2024-02-08 20:48:54 -0700

    modifcación de nombre sitio

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-   <h1 className="title">
-      Optimus Motors  <span aria-label="emoji" role="img">🔥</span>
+   <h1 className="title">Optimus Motors<span aria-label="emoji" role="img">🔥</span>
    </h1>
    <input type="text" className="new-task"/>
 </div>
