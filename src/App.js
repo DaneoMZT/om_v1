dee694978a885040477905abee372263fc3d3737
DaneoMZT <danielruizbeltran@MacBook-Air-de-Daniel.local>
2024-02-08 20:26:33 -0700

    optimus_Motor_V1

--- src/App.js
+++ src/App.js
@@ -3,23 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>Desde VSCODE</h1>
-      </header>
-    </div>
+    <div className="container">
+   <h1 className="title">
+      Hello world <span aria-label="emoji" role="img">🔥</span>
+   </h1>
+   <input type="text" className="new-task"/>
+</div>
   );
 }
 
